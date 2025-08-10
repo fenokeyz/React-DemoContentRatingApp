12c39e3ce7a7a57b7180727cbd17b977d05dc2de
nav <navaneethsuresh21@gmail.com>
2025-08-10 01:48:30 -0400

    final commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,41 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        totalRatings: 0,
+        handleLike:() => {
+            this.setState((prevState) => ({
+                likes: prevState.likes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }));
+        },
+        handleDislike:() => {
+            this.setState((prevState) => ({
+                dislikes: prevState.dislikes + 1,
+                totalRatings: prevState.totalRatings + 1
+            }));
+        }
+    };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+        <p>
+            Do you like or dislike the Current Political,Economical State of the World right now?<br/>
+            <img src='https://media1.tenor.com/m/ly3NOxbUZXoAAAAd/jaden-smith.gif'alt='Jaden Man'></img>
+        </p>
+        <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+                Like({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+                Dislike({this.state.dislikes})
+            </button>
+        </div>
+        <p>Total Ratings: {this.state.totalRatings} </p>
+     </div>
     );
   }
 }
